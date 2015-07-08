c63d6f6cf4c706ef469f7703e92bcfbf430446ea
Niels4P <4porsiel@informatik.uni-hamburg.de>
2015-07-08 17:13:18 +0200

    Orientation works, yey :)

--- main.cpp
+++ main.cpp
@@ -14,6 +14,9 @@
 #include <vigra/matrix.hxx>
 #include <vigra/regression.hxx>
 #include <vigra/quadprog.hxx>
+#include <vigra/gaussians.hxx>
+#include <vigra/resampling_convolution.hxx>
+#include <vigra/basicgeometry.hxx>
 
 inline double log2(double n)
 {
@@ -25,10 +28,11 @@ inline double log2(double n)
  */
 struct dogFeature
 {
-    float x;    //y-coord
-    float y;    //x-coord
+    float x;    //x-coord
+    float y;    //y-coord
     float s;    //scale (sigma)
     float m;    //magnitude of DoG
+	double a;    //angle
 };
 
 /**
@@ -63,37 +67,54 @@ inline bool localExtremum(const std::vector<vigra::MultiArray<2, float> > & dog,
                  &&  my_val > dog[i-1](x-1,y+1) && my_val > dog[i-1](x,y+1) && my_val > dog[i-1](x+1,y+1)));
 }
 
+/**
+ * Inline function to determine the exact angle of orientation
+ *
+ */
+inline void calcParabolaVertex(int x1, int y1, int x2, int y2, int x3, int y3, double& xv) //double& yv
+{
+	double denom = (x1 - x2) * (x1 - x3) * (x2 - x3);
+	double A     = (x3 * (y2 - y1) + x2 * (y1 - y3) + x1 * (y3 - y2)) / denom;
+	double B     = (x3*x3 * (y1 - y2) + x2*x2 * (y3 - y1) + x1*x1 * (y2 - y3)) / denom;
+	double C     = (x2 * x3 * (x2 - x3) * y1 + x3 * x1 * (x3 - x1) * y2 + x1 * x2 * (x1 - x2) * y3) / denom;
+
+	xv = -B / (2*A);
+	//yv = C - B*B / (4*A);
+}
+
+/**
+ * Function to locate extrema on subpixel-level
+ * using first derivative and hessian matrix
+ */
 bool subpixel(const std::vector<vigra::MultiArray<2, float> > & dog, int i, int x, int y, float threshold, float ratio, float height, float width, float& off_x, float& off_y, float& off_s)
 {
 		using namespace vigra;
 		using namespace vigra::linalg;
 
 		int s = i-2;
+		// derivative of first order
 		float dx = (dog[s](x+1,y)-dog[s](x-1,y))/2;
 		float dy = (dog[s](x,y+1)-dog[s](x,y-1))/2;
 		float ds = (dog[s+1](x,y)-dog[s-1](x,y))/2;
 
-
-		if (sqrt(dog[s](x,y)+(pow(dx,2)+pow(dy,2)+pow(ds,2))*1.5f)<threshold*256)
-		{
-			return false;
-		}
-
+		// partial derivatives of second order
 		float d2 = 2*dog[s](x,y);
 		float dxx = dog[s](x+1,y)+dog[s](x-1,y)-d2;
 		float dyy = dog[s](x,y+1)+dog[s](x,y-1)-d2;
 		float dss = dog[s+1](x,y)+dog[s-1](x,y)-d2;
 
-		float dxy = (dog[s](x+1,y+1)-dog[s](x-1,y+1)-dog[s](x+1,y-1)+dog[s](x-1,y-1))/2;
-		float dxs = (dog[s+1](x+1,y)-dog[s+1](x-1,y)-dog[s-1](x+1,y)+dog[s-1](x-1,y))/2;
-		float dys = (dog[s+1](x,y+1)-dog[s+1](x,y-1)-dog[s-1](x,y+1)+dog[s-1](x,y-1))/2;
+		float dxy = (dog[s](x+1,y+1)-dog[s](x-1,y+1)-dog[s](x+1,y-1)+dog[s](x-1,y-1))/4;
+		float dxs = (dog[s+1](x+1,y)-dog[s+1](x-1,y)-dog[s-1](x+1,y)+dog[s-1](x-1,y))/4;
+		float dys = (dog[s+1](x,y+1)-dog[s+1](x,y-1)-dog[s-1](x,y+1)+dog[s-1](x,y-1))/4;
 
+		// hessian matrix
 		float H_data[] = {
 			 dxx, dxy, dxs,
 			 dxy, dyy, dys,
 			 dxs, dys, dss
 		};
 
+		// vector with derivatives of first order
 		float D_data[] = {
 			dx,
 			dy,
@@ -104,32 +125,129 @@ bool subpixel(const std::vector<vigra::MultiArray<2, float> > & dog, int i, int 
 		Matrix<float> D(Shape2(3,1), D_data);
 		Matrix<float> offset(Shape2(3,1));
 
+		// trace of the hessian to the power of 2 divided by the determinant of the hessian
 		float soe = pow(trace(H),2)/determinant(H);
 		
+		//elimination of keypoints with a ratio below the given value between the principal curvatures
 		if (soe>=(pow((ratio+1),2))/ratio){
 			return false;
 		}
 		
+		// calculation of the offset in every dimension (x,y,sigma)
 		bool solution = linearSolve(H, D, offset);
 		if (solution){
+			// rejection of unstable extrema with low contrast below the threshold
+			if ((std::abs(dog[s](x,y)) + dot(D.transpose(),-offset)*0.5) < threshold*256){
+				return false;
+			}
+
 			off_x = offset(0,0);
 			off_y = offset(1,0);
 			off_s = offset(2,0);
-
+			
+			// no change of offset, if keypoint is at the edge of any dimension
 			if ((x == 1 && off_x >= 0.5f) || (x == width && off_x <= -0.5f))
 				off_x = 0.0f;
 			if ((y == 1 && off_y >= 0.5f) || (y == height && off_y <= -0.5f))
 				off_y = 0.0f;
-
+			
+			// keypoint will be added
 			return true;
 		}
 		return false;
 
-
-		//TODO:
-		//offset nicht größer als 0,5: wie sieht das bei sigma aus?
-		//negative Koordinaten?! siehe Subpixel- und Rechteckbilder
 }
+
+/**
+ * Inline function to determine the exact angle of orientation
+ *
+ */
+bool orientation(const std::vector<vigra::MultiArray<2, float> > & octave, int i, int x, int y, double& angle, double& angle2){
+	using namespace std;
+	using namespace vigra;
+	using namespace vigra::multi_math;
+
+	// Gaussian weighted circular window (8 pixel radius)
+	Gaussian<double> gauss(8.0/3.0);
+	
+	vector<double> bin(36);
+
+	//check, if keypoint is too close to the edge of any dimension
+	if (x < 8 || x > octave[i].width() - 8)
+		return false;
+
+	if (y < 8 || y > octave[i].height() - 8)
+		return false;
+
+	// orientation of neighbouring pixel is weighted by the gaussian and its magnitude and added to a histogram (bin-array)
+	for(int yo=-7; yo<=8; ++yo){
+		for(int xo=-7; xo<=8; ++xo){
+			double r = sqrt(xo*xo + yo*yo);
+			double weight = gauss(r);
+
+			double magnitude = sqrt(pow(octave[i](x+xo+1,y+yo)-octave[i](x+xo-1,y+yo),2)+pow(octave[i](x+xo,y+yo+1)-octave[i](x+xo,y+yo-1),2));
+			int theta = int(((atan2(octave[i](x+xo,y+yo+1)-octave[i](x+xo,y+yo-1), octave[i](x+xo+1,y+yo)-octave[i](x+xo-1,y+yo))+M_PI)/M_PI)*18);
+
+			bin[theta] += magnitude * weight;
+		}
+	}
+
+	double peak_value = 0.0, peak2_value = 0.0;
+	int peak_element = 0, peak2_element = 0;
+	bool found2 = false;
+
+	//searching for the peak value in the histogram
+	for(int b=0; b<36; ++b){
+		if (bin[b]>peak_value){
+			peak_value = bin[b];
+			peak_element = b;
+		}
+	}
+
+	//check for a second peak element with at least 80% value of the peak element
+	//-> will be added as an extra feature point with exact same coordinates but different angle
+	for(int b=0; b<36; ++b){
+		if (b==peak_element){
+			continue;
+		} else if (bin[b]>peak_value*0.8){
+			peak2_value = bin[b];
+			peak2_element = b;
+			found2 = true;
+		}
+	}
+
+	//neighbours of peak element will be needed for parabola computation of the exact angle
+	int n1, n2;
+	n1 = peak_element - 1;
+	n2 = peak_element + 1;
+	if (n1 < 0){
+		n1 = 35;
+	}
+	if (n2 > 35){
+		n2 = 0;
+	}
+
+	//calculation of exact angle with parabola computation
+	//5 degrees is added to every bin to use the mean of every bin
+	calcParabolaVertex(n1*10+5,bin[n1],peak_element*10+5,bin[peak_element],n2*10+5,bin[n2],angle);
+
+	//a second peak element was found and needs to be calculated like the first
+	if (found2){
+		n1 = peak2_element - 1;
+		n2 = peak2_element + 1;
+		if (n1 < 0){
+			n1 = 35;
+		}
+		if (n2 > 35){
+			n2 = 0;
+		}
+		calcParabolaVertex(n1*10+5,bin[n1],peak2_element*10+5,bin[peak2_element],n2*10+5,bin[n2],angle2);
+	}
+
+	return true;
+
+}
+
 
 /**
  * The main method - will be called at program execution
@@ -208,7 +326,7 @@ int main(int argc, char** argv)
         //Further parameters
         int         s = 2;
         float       k = pow(2.0,1.0/s);
-        int intervals = s+3;
+        const int  intervals = s+3;
         
         //Data containers:
         vector<MultiArray<2, float> > octave(intervals);
@@ -217,19 +335,25 @@ int main(int argc, char** argv)
         
         //initialise first octave with current (maybe doubled) image
         octave[0] = image;
+
+		//vector to store the sigma-value of every scale
+		vector<double> scales;
         
         //Run the loop
         for(int o=0; o<octaves; ++o)
         {
             string file_basename = string("sift_octave") + boost::lexical_cast<string>(o);
             //exportImage(octave[0], file_basename+ + "_level0.png");
+
+			//clear for every octave and add zero element
+			scales.clear();
+			scales.push_back(0.0);
             
             for (int i=1; i<intervals; ++i)
             {
                 //initialize curent interval image array
                 octave[i].reshape(octave[i-1].shape());
-                
-                
+
                 //Create each interval step image using the previous one
                 if(iterative_interval_creation)
                 {
@@ -239,12 +363,16 @@ int main(int argc, char** argv)
                     double last_sigma  = pow(k, i-1.0)*sigma,
                            total_sigma = last_sigma*k;
                     
-                    gaussianSmoothing(octave[i-1], octave[i], sqrt(total_sigma*total_sigma - last_sigma*last_sigma));
+					//scales[i] = sqrt(total_sigma*total_sigma - last_sigma*last_sigma);
+					scales.push_back(sqrt(total_sigma*total_sigma - last_sigma*last_sigma));
+                    gaussianSmoothing(octave[i-1], octave[i], scales[i]);
                 }
                 //Create each interval step using the base image
                 else
                 {
-                    gaussianSmoothing(octave[0], octave[i],  pow(k,(double)i)*sigma);
+					//scales[i] = pow(k,(double)i)*sigma;
+					scales.push_back(pow(k,(double)i)*sigma);
+                    gaussianSmoothing(octave[0], octave[i], scales[i]);
                 }
                 
                 //Compute the dog
@@ -257,7 +385,6 @@ int main(int argc, char** argv)
                     //Determine current sigma of this dog step at this octave:
                     double current_sigma  = o*sigma + pow(k, i-2.0)*sigma;
 
-					//logk(current_sigma/sigma - o) + 2.0 = i
                     
                     for (int y=1; y<dog[i-2].height()-1; ++y)
                     {
@@ -266,14 +393,47 @@ int main(int argc, char** argv)
                             float my_val = dog[i-2](x,y);
                             if ( abs(my_val) > dog_threshold && localExtremum(dog, i,x,y))
                             {
+								//calling subpixel function to determine subpixel-offset of extrema
 								float off_x, off_y, off_s;
 								if (subpixel(dog,i,x,y,threshold,ratio,dog[i-2].height()-1,dog[i-2].width()-1,off_x,off_y,off_s)){
 
-									dogFeature new_feature = { (x-off_x)*pow(2,o+o_offset),
-															   (y-off_y)*pow(2,o+o_offset),
-															   (current_sigma-off_s),
-															   abs(my_val)};
-									dogFeatures.push_back(new_feature);
+									int scale = 0;
+									double sigscale = 10.0;
+									//calculating the scale, where to look for orientation
+									//sigma of dog, where the extrema was found, has to be used to find the matching image with similar sigma in octave array
+									for(int s=0; s<scales.size(); ++s){
+										double check = abs(scales[s]-(current_sigma-off_s));
+										if (sigscale > check){
+											sigscale = check;
+											scale = s;
+										}
+									}
+									
+									double angle, angle2;
+									bool save;
+									//calculation of orientation of keypoint
+									//keypoint will be dismissed, if save is false
+									//may cause the need to add a second one (if angle2 is given back)
+									save = orientation(octave, scale, x, y, angle, angle2);
+
+									//adding keypoint(s) to list of feature keypoints
+									if (save) {
+										dogFeature new_feature = { (x-off_x)*pow(2,o+o_offset),
+																   (y-off_y)*pow(2,o+o_offset),
+																   (current_sigma-off_s),
+																   abs(my_val),
+																   angle};
+										dogFeatures.push_back(new_feature);
+									
+										if (angle2){
+											dogFeature new_feature = { (x-off_x)*pow(2,o+o_offset),
+																	   (y-off_y)*pow(2,o+o_offset),
+																	   (current_sigma-off_s),
+																	   abs(my_val),
+																	   angle2};
+											dogFeatures.push_back(new_feature);
+										}
+									}
 								}
                             }
                         }
@@ -299,16 +459,18 @@ int main(int argc, char** argv)
         cout << "Found: " << dogFeatures.size() << " candidates.\n";
 		*/
 
+		//output will be in scalable vector graphics format
+		//with link to original image file in same directory
 		cout << "<svg height=\"" << height << "\" width=\"" << width << "\">\n";
 		cout << "<g>\n" << "<image y=\"0.0\" x=\"0.0\" xlink:href=\"" << image_filename << "\" height=\"" << height << "\" width=\"" << width << "\" />\n" << "</g>\n";
 		cout << "<g>\n";
 		for(const dogFeature& f : dogFeatures)
         {
-			cout << "<rect x=\"" << f.x-(f.s/2) << "\" y=\"" << f.y-(f.s/2) << "\" height=\"" << f.s << "\" width=\"" << f.s << "\" stroke=\"yellow\" fill=\"none\" />\n";
+			cout << "<rect x=\"" << -(f.s/2) << "\" y=\"" << -(f.s/2) << "\" height=\"" << f.s << "\" width=\"" << f.s << "\" stroke=\"red\" fill=\"none\" transform=\"translate(" << f.x << "," << f.y << ") rotate(" << f.a << ")\" />\n";
         }
 		cout << "</g>\n";
 		cout << "</svg>"; 
-
+		//transform=\"rotate(" << f.a << "," << f.x-(f.s/2) << "," << f.y-(f.s/2) << ")\"
 
     }
     catch(po::required_option& e)
